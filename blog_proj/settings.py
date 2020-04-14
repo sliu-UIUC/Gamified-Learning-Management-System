12a20f59592fe44e288e3a425b72397f22a35042
Shiyu <sliu@trinity.edu>
2020-04-14 10:22:50 -0500

    edit stuff

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '94xa+wc1v2^y5y#u@qwa70@j5i4i(!*-ya&%79&*qm90c%-nck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
