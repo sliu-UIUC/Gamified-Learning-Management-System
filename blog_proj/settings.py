9a5af029f1f2c92a41826391a42a48bc6872d78a
Shiyu <sliu@trinity.edu>
2020-04-14 02:26:08 -0500

    Updated STATIC_ROOT

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -129,6 +129,7 @@ INVENTORY_SESSION_ID = 'userInventory'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
